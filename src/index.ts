c0b3826ca4bb950af8a5e43718ca76a3632e3a7d
Lucas Zottis <zottis.lucas@hotmail.com>
2026-03-28 08:55:15 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -28,4 +28,3 @@ export * from './converters/numeric-systems/numericSystem.converter'
 export * from './converters/time-decimal/models/conversion-result.model';
 export * from './converters/time-decimal/interfaces/ITimeDecimal.converter';
 export * from './converters/time-decimal/time-decimal.converter';
-export * from './converters/text-format/textFormatConverter.converter';
